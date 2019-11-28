e9be15ec40e81b7a5f08ea9be667b3c23ec2826b
Garrett Dancik <garrett.dancik@gmail.com>
2019-11-28 13:05:04 -0500

    Update

---
+++ msg.js
@@ -0,0 +1,18 @@
+
+down = false;
+down = true;
+
+msgCPP = '<i>CPP</i> is available from <a href = "http://bioinformatics.easternct.edu/app/CPP/">http://bioinformatics.easternct.edu/app/CPP/</a>';
+
+msgBCBET = '<ul>' + 
+ '<li> Analysis of <b>gene <i> expression</i> biomarkers</b> using <i> BC-BET</i> is available at the following link: <a href = "http://bioinformatics.easternct.edu/BCBET2/"> http://bioinformatics.easternct.edu/BCBET2/</a></li>' + 
+'<li> Analysis of <b> gene <i> methylation</i> biomarkers</b> using <i> BC-BET</i> is available at the following link: <a href = "http://bioinformatics.easternct.edu/BCBET-Methyl/"> http://bioinformatics.easternct.edu/BCBET-Methyl/</a></li </ul>';
+
+if (down) {
+    msgCPP = 'Due to a university-wide web server reboot, <i>CPP</i> is temporarily unavailable. We apologize for the inconvenience and expect <i>CPP</i> to be available again shortly.';
+
+msgBCBET = 'Due to a university-wide web server reboot, <i>BC-BET</i> is temporarily unavailable. We apologize for the inconvenience and expect <i>BC-BET</i> to be available again shortly.';
+
+}
+
+
