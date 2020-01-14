415d8514a99169e80828bd9638c0d669e0f4ce2b
Garrett Dancik <garrett.dancik@gmail.com>
2020-01-14 17:49:13 -0500

    Update

--- msg.js
+++ msg.js
@@ -6,7 +6,7 @@ msgCPP = '<i>CPP</i> is available from <a href = "http://bioinformatics.easternc
 
 msgBCBET = '<ul>' + 
  '<li> Analysis of <b>gene <i> expression</i> biomarkers</b> using <i> BC-BET</i> is available at the following link: <a href = "http://bioinformatics.easternct.edu/BCBET2/"> http://bioinformatics.easternct.edu/BCBET2/</a></li>' + 
-'<li> Analysis of <b> gene <i> methylation</i> biomarkers</b> using <i> BC-BET</i> is available at the following link: <a href = "http://bioinformatics.easternct.edu/BCBET-Methyl/"> http://bioinformatics.easternct.edu/BCBET-Methyl/</a></li </ul>';
+'<li> Analysis of <b> gene <i> methylation</i> biomarkers</b> using <i> BC-BET</i> is available at the following link: <a href = "http://bioinformatics.easternct.edu/app/BCMethyl/"> http://bioinformatics.easternct.edu/BCBET-Methyl/</a></li </ul>';
 
 if (down) {
     msgCPP = '<i>CPP</i> is temporarily undergoing maintenance.. We apologize for the inconvenience and expect <i>CPP</i> to be available again shortly.';
