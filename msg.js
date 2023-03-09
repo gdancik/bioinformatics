f65669a569056149f26069b8036d35dbd7bce076
Garrett Dancik <garrett.dancikg@gmail.com>
2023-03-09 17:33:35 -0500

    Update

--- msg.js
+++ msg.js
@@ -5,8 +5,9 @@ down = false;
 msgCPP = '<i>CPP</i> is available from <a href = "https://bioinformatics.easternct.edu/app/CPP/">https://bioinformatics.easternct.edu/app/CPP/</a>';
 
 msgBCBET = '<ul>' + 
- '<li> Analysis of <b>gene <i> expression</i> biomarkers</b> using <i> BC-BET</i> is available at the following link: <a href = "http://bioinformatics.easternct.edu/BCBET2/"> http://bioinformatics.easternct.edu/BCBET2/</a></li>' + 
-'<li> Analysis of <b> gene <i> methylation</i> biomarkers</b> using <i> BC-BET</i> is available at the following link: <a href = "http://bioinformatics.easternct.edu/app/BCMethyl/"> http://bioinformatics.easternct.edu/BCBET-Methyl/</a></li </ul>';
+ '<li> Analysis of <b>gene <i> expression</i> biomarkers</b> using <i> BC-BET</i> is available at the following link: <a href = "https://bioinformatics.easternct.edu/app/bcbet"> https://bioinformatics.easternct.edu/app/bcbet</a></li>' + 
+'<li> Source code for BC-BET is available at the following link: <a href = "https://github.com/gdancik/BC-Methyl"> https://github.com/gdancik/BC-Methyl</a></li </ul>';
+'<li> Source code for BC-Methlyl is available at the following link: <a href = "https://github.com/gdancik/BC-Methyl"> https://github.com/gdancik/BC-Methyl</a></li </ul>';
 
 if (down) {
     msgCPP = '<i>CPP</i> is temporarily undergoing maintenance.. We apologize for the inconvenience and expect <i>CPP</i> to be available again shortly.';
