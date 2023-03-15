13f5f802f56e69c413683ab0d217939f17945d3a
Garrett Dancik <garrett.dancikg@gmail.com>
2023-03-15 15:53:46 -0400

    Update BC-Methyl links

--- msg.js
+++ msg.js
@@ -6,8 +6,7 @@ msgCPP = '<i>CPP</i> is available from <a href = "https://bioinformatics.eastern
 
 msgBCBET = '<ul>' + 
  '<li> Analysis of <b>gene <i> expression</i> biomarkers</b> using <i> BC-BET</i> is available at the following link: <a href = "https://bioinformatics.easternct.edu/app/bcbet"> https://bioinformatics.easternct.edu/app/bcbet</a></li>' + 
-'<li> Source code for BC-BET is available at the following link: <a href = "https://github.com/gdancik/BC-Methyl"> https://github.com/gdancik/BC-Methyl</a></li </ul>';
-'<li> Source code for BC-Methlyl is available at the following link: <a href = "https://github.com/gdancik/BC-Methyl"> https://github.com/gdancik/BC-Methyl</a></li </ul>';
+'<li> Analysis of <b> methylation biomarkers </b> is available at the following link: <a href = "https://github.com/gdancik/BC-Methyl"> https://github.com/gdancik/BC-Methyl</a></li </ul>';
 
 if (down) {
     msgCPP = '<i>CPP</i> is temporarily undergoing maintenance.. We apologize for the inconvenience and expect <i>CPP</i> to be available again shortly.';
